475286153ca6106449b439e76b5a3da9f12112e0
Kyle Martens <martensk42@gmail.com>
2014-11-23 17:54:27 -0800

    trying yet again to commit my Paper and PaperTest classes

--- Development-Project/src/model/Paper.java
+++ Development-Project/src/model/Paper.java
@@ -1,9 +1,12 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * This method establishes the characteristics of a paper.
+ * This class establishes the characteristics of a paper.
  * 
  * @author Kim Stuart
  * @author Kyle Martens
@@ -13,171 +16,206 @@ import java.io.Serializable;
  *
  */
 
-public class Paper implements Serializable {
-
+public class Paper implements Serializable{
+	
+	/**
+	 * the Title of the paper
+	 */
     private String myTitle;
+	
+    /**
+     * the file path of the paper on the local file system
+     */
+    private String myFilePath;
     
-    private String myPathFile;
+    /**
+     * a map of reviewers to their respective review file paths
+     */
+    private Map<String, String> myReviews;
+	
+    /**
+     * the file path for the paper's recommendation
+     */
+    private String myRecommendation;
+	
+    /**
+     * whether the paper is assigned to a SPC
+     */
+    private boolean myAssignedToSPC;
+	
+    /**
+     * whether the paper has a submitted recommendation
+     */
+    private boolean myIsRecommended;
+	
+    /**
+     * the rating given to the paper by the SPC
+     */
+    private int myRating;
     
-	private String mySPC;
-    
-    private String[] myReviewers;
-    
-    private String myReview;
-    
-    private String[] myReviews;
+    /**
+     * the acceptance status of the paper
+     */
+    private int myAccepted;
 	
-	private String myRecommendation;
-	
-	private boolean myAssignedToReviewer;
-	
-	private boolean myAssignedToSPC;
-	
-	private boolean myIsReviewed;
-	
-	public int myAccepted;
-	
-	
+    /**
+     * Constructor for a paper
+     * @param theTitle the title of the paper
+     * @param theFilePath the file path on the local system for the paper
+     */
+	public Paper(String theTitle, String theFilePath) {
+		this(theTitle, theFilePath, new HashMap<String, String>(), null,
+			false, false, -1, 0);
+	}
 	
 	/**
 	 * Constructor for a paper.
-	 * @param theReview initially the paper has not yet been reviewed so it is null.
-	 * @param theRecommendation initially the paper has not been given a recommendation so it is null.
-	 * @param theAssignedToReviewer  initially the paper has not been assigned to a reviewer to review so its status is false.
-	 * @param theAssignedToSPC initially the PC has not assigned this paper to a SPC so its status is false.
-	 * @param theIsReviewed initially the paper has not been reviewed so it is false.
-	 * @param theAccepted  initially the PC has not made a decision until is goes through the review and scoring process, so its int is 0 to represent undecided.
+     * @param theTitle the title of the paper
+     * @param theFilePath the file path on the local system for the paper
+	 * @param theReviews the map of reviewers to their reviews
+	 * @param theRecommendation the recommendation for the paper
+	 * @param theAssignedtoSPC whether the paper is assigned to a SPC
+	 * @param theIsRecommended whether the paper is recommended
+	 * @param theRating the rating given to the paper by the SPC
+	 * @param theIsAccepted whether the paper is accepted by the PC
 	 */
-	public Paper(String theTitle, String thePathFile) {
-		
+	public Paper(String theTitle, String theFilePath, Map<String, String> theReviews,
+				String theRecommendation, boolean theAssignedtoSPC, boolean theIsRecommended,
+				int theRating, int theIsAccepted) {
 		myTitle = theTitle;
-		myPathFile = thePathFile;
-		myReview = null;
-		myRecommendation = null;
-		myAssignedToReviewer = false;
-		myAssignedToSPC = false;
-		myIsReviewed = false;
-		myAccepted = 0;
-	    mySPC = null;
-	
-		myReviewers = new String[4];
-		myReviews = new String[5];
-	}
-	
-	public String getSPC() {
-		return mySPC;
-	}
-	
-	public void setMySPC(final String theSPC){
-		mySPC = theSPC;
+		myFilePath = theFilePath;
+		myReviews = theReviews;
+		myRecommendation = theRecommendation;
+		myAssignedToSPC = theAssignedtoSPC;
+		myIsRecommended = theIsRecommended;
+		myRating = theRating;
+		myAccepted = theIsAccepted;
 	}
 	
 	/**
-	 * this method will determine if the paper has been assigned to a reviewer.
-	 * @return false if the paper has not been assigned to a reviewer or true if it has.
+	 * this method will assign a reviewer to this paper.
+	 * @param theReviewer
 	 */
-	public boolean setAssignedToReviewer(final String theReviewer) {
-		boolean result = false;
-		if(myReviewers[0] == null) {
-			myReviewers[0] = theReviewer;
-			result = true;
-		} else if(myReviewers[0] != null) {
-			myReviewers[1] = theReviewer;
-			result = true;
-		}else if (myReviewers[1] != null) {
-		    myReviewers[2] = theReviewer;
-		    result = true;
-		} else if (myReviewers[2]!= null) {
-			myReviewers[3] = theReviewer;
-			result = true;
-		}else if(myReviewers[3] != null){
-			System.out.println("This paper cannot accept any more reviewers");
-			result = true;
+	public void assignToReviewer(String theReviewer) {
+		if(theReviewer != null) {
+			myReviews.put(theReviewer, null);
 		}
-		return result;
 	}
 	
 	/**
 	 * this method will determine if the paper has been assigned to a SPC.
-	 * @return false if the paper has not been assigned to a SPC or true if it has.
 	 */
-	public void setAssignedToSPC(final String theSPC) {
-		if(myAssignedToSPC == true) {
-			System.out.println("A Subprogram Chair has already been assigned to this paper.");
-		} else {
-			mySPC = theSPC;
-			myAssignedToSPC = true;
-		}
+	public void assignToSPC(boolean isAssigned) {
+		myAssignedToSPC = isAssigned;
 	}
 	
 	/**
-	 * Getter method to return is an SPC has been assigned to a paper.
-	 * @return boolean true if a SPC has been assigned and false if a SPC has not been assigned to a paper.
+	 * @return whether this paper has been assigned to the given reviewer.
+	 */
+	public boolean isAssignedToReviewer(String theReviewer) {
+		return myReviews.containsKey(theReviewer);
+	}
+	
+	/**
+	 * @return whether this paper has been assigned to a SPC
 	 */
 	public boolean isAssignedToSPC() {
 		return myAssignedToSPC;
 	}
 	
 	/**
+	 * @return whether the paper has been reviewed by the given reviewer.
+	 */
+	public boolean isReviewed(String theReviewer) {
+		return myReviews.get(theReviewer) != null;
+	}
+	
+	/**
 	 * this method will determine if the paper has been reviewed by the reviewer.
 	 * @return false if the paper has been reviewed or true if it has.
 	 */
-	public boolean isReviewed() {
-		return myIsReviewed;
-	}
-	
-	public boolean getMyAssignedToReviewer() {
-		return myAssignedToReviewer;
-	}
-	
-	public void setMyAssignedToReviewer() {
-		myAssignedToReviewer = true;
+	public boolean isRecommended() {
+		return myIsRecommended;
 	}
 	
 	/**
 	 * this method will allow the PC to set the status of a paper based on his or her decision.
 	 * @param theDecision -1 if rejected, 0 if undecided or 1 if accepted.
 	 */
-	public void setStatus(final String theDecision) {
-		if (theDecision.equals("accepted".toLowerCase())) {
-			myAccepted = 1;
-		} else if (theDecision.equals("rejected".toLowerCase())) {
-			myAccepted = -1;
-		}else {
-			myAccepted = 0;
-		}
+	public void updateStatus(int theStatus) {
+		myAccepted = theStatus;
 	}
 	
 	/**
-	 * this method will allow the SPC to claim a paper to be reviewed
+	 * This method will submit the review of a reviewer.
+	 * @param theUserName the user name of the reviewer
+	 * @param theFilePath the file path for the review
 	 */
-	public void assignReview(final String theUserName, final String theFilePath) {
-		myReview = theFilePath;
-		myIsReviewed = true;
+	public void assignReview(String theReviewer, String theFilePath) {
+		if(theReviewer != null) {
+			myReviews.put(theReviewer, theFilePath);
+		}
+	}
+
+	/**
+	 * this method will submit the recommendation of the SPC.
+	 * @param theFilePath the path in the directory where the recommendation is stored.
+	 * @param rating the rating given to this paper
+	 */
+	public void assignRecommendation(String theFilePath, int theRating) {
+		if(theFilePath != null) {
+			myRecommendation = theFilePath;
+			myRating = theRating;
+			myIsRecommended = true;
+		}
+	}
+
+	/**
+	 * @return the Title of the paper
+	 */
+	public String getTitle() {
+		return myTitle;
+	}
+
+	/**
+	 * @return the file path of the paper
+	 */
+	public String getFilePath() {
+		return myFilePath;
 	}
 	
-	public String getMyRecommendation() {
+	/**
+	 * @return the reviews for this paper
+	 */
+	public Collection<String> getReviews() {
+		return myReviews.values();
+	}
+
+	/**
+	 * @return the myRecommendation
+	 */
+	public String getRecommendation() {
 		return myRecommendation;
 	}
 	
-	public void setMyRecommendation(final String theRecommendation) {
-		myRecommendation = theRecommendation;
+	/**
+	 * @return the rating given to this paper
+	 */
+	public int getRating(){
+		return myRating;
 	}
 	
 	/**
-	 * this method will allow the Reviewer to make a recommendation to the SPC or the SPC to make a recommendation to the PC.
-	 * @param theUsername ?
-	 * @param theFilePath the path in the directory where the recommendation is stored.
+	 * @return the acceptance status of the paper
 	 */
-	public void assignRecommendation(final String theUsername, final String theFilePath) {
-		myRecommendation = theFilePath;
-		myAssignedToSPC = true;
+	public int getStatus() {
+		return myAccepted;
 	}
 	
+	/**
+	 * @return a string representation of the paper
+	 */
 	public String toString() {
-		String str = "";
-		str = str + myTitle;
-		
-		return str;
+		return myTitle;
 	}
 }
--- Development-Project/src/test/PaperTest.java
+++ Development-Project/src/test/PaperTest.java
@@ -1,35 +1,93 @@
 package test;
 
-import static org.junit.Assert.*;
-import model.Conference;
+import java.util.HashMap;
+import java.util.Map;
+
 import model.Paper;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class PaperTest {
-
-	Conference test;
-	Paper paper;
+	
+	/**
+	 * a few papers used for testing
+	 */
+	private Paper paper, paper2, paper3, paper4;
+	
+	/**
+	 * constructing the paper objects for use later on
+	 */
 	@Before
 	public void setup()
 	{
-//		test = new Conference("conference1");
-//		test.assignRole("Stevan", "Spc");
-//		test = new Conference("conference1", "ACFowler");
-//		test.assignRole("Stevan", "Spc");
+		paper = new Paper("The Adventures of Alan Fowler", "C:\\idk");
+		paper2 = new Paper(null, null);
+		paper3 = new Paper("Random Title","C:\\Users\\Kyle\\My Documents\\Random Title",
+				new HashMap<String, String>(), "C:\\recommendation", true, true, 4, 1);
+		
+		Map<String, String> reviews = new HashMap<String, String>();
+		reviews.put("Kyle Martens", "D:\\KyleReview");
+		reviews.put("Kim Stuart", "D:\\KimReview");
+		reviews.put("Mick Johnson", "D:\\MickReview");
+		reviews.put("Stevan Lewis", "D:\\StevanReview");
+		paper4 = new Paper("The Adventures of Alan Fowler pt 2", "D:\\TopSecret",
+				reviews, "D:\\Recommended", true, true, 2, -1);
+	}
+	
+	/**
+	 * test code for the assignToReviewer method
+	 */
+	@Test
+	public void testAssignToReviewer() {
+		
 	}
 
+	/**
+	 * test code for the assignToSPC method
+	 */
+	@Test
+	public void testAssignToSPC() {
+		
+	}
+
+	/**
+	 * test code for the isReviewed method
+	 */
+	@Test
+	public void testIsReviewed() {
+		
+	}
+
+	/**
+	 * test code for the isRecommended method
+	 */
+	@Test
+	public void testIsRecommended() {
+		
+	}
+
+	/**
+	 * test code for the updateStatus method
+	 */
+	@Test
+	public void testUpdateStatus() {
+		
+	}
+
+	/**
+	 * test code for the assignReview method
+	 */
 	@Test
 	public void testAssignReview() {
-		Paper paper1 = new Paper("The Adventures of Alan Fowler", "c:/idk");
-		Paper paper2 = new Paper(null, null);
 		
-		paper1.assignRecommendation("johnsmith1", "c:/**");
-		
-//		assertEquals(paper1.myReview, "c:/**");
-//		assertEquals(paper2.myReview, null);
-//		assertEquals(paper1.myIsReviewed, true);
 	}
 
+	/**
+	 * test code for the assignToRecommendation method
+	 */
+	@Test
+	public void testAssignRecommendation() {
+		
+	}
 }
